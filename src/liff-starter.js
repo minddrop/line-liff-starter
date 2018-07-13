190b48edff4ee2767d7b3987545f1a35937c114e
ryu <ryooooooo1@gmail.com>
2018-07-13 11:07:33 +0900

    db接続＆liffとheroku捨てた

--- src/liff-starter.js
+++ src/liff-starter.js
@@ -1,74 +1,97 @@
-window.onload = function (e) {
-    liff.init(function (data) {
-        initializeApp(data);
-    });
-};
+document.addEventListener('DOMContentLoaded', function () {
+    const roomTest = firebase.firestore().collection('chat-test')
+    document.querySelector('#submit').addEventListener('click', (event) => {
+        const chatBody = document.querySelector('#body')
+        const chatName = document.querySelector('#name')
+        if (!chatBody.value) alert('Enter your message')
+        else {
+            roomTest.add(
+                {
+                    body: chatBody.value,
+                    date: Date.now(),
+                    name: chatName.value,
+                }
+            ).then(
+                () => {
+                    chatBody.value = "";
+                    chatName.value = "";
+                }
+            ).catch(e => alert(e))
+        }
+    })
+})
 
-function initializeApp(data) {
-    document.getElementById('languagefield').textContent = data.language;
-    document.getElementById('viewtypefield').textContent = data.context.viewType;
-    document.getElementById('useridfield').textContent = data.context.userId;
-    document.getElementById('utouidfield').textContent = data.context.utouId;
-    document.getElementById('roomidfield').textContent = data.context.roomId;
-    document.getElementById('groupidfield').textContent = data.context.groupId;
+// window.onload = function (e) {
+//     liff.init(function (data) {
+//         initializeApp(data);
+//     });
+// };
 
-    // openWindow call
-    document.getElementById('openwindowbutton').addEventListener('click', function () {
-        liff.openWindow({
-            url: 'https://line.me'
-        });
-    });
+// function initializeApp(data) {
+//     document.getElementById('languagefield').textContent = data.language;
+//     document.getElementById('viewtypefield').textContent = data.context.viewType;
+//     document.getElementById('useridfield').textContent = data.context.userId;
+//     document.getElementById('utouidfield').textContent = data.context.utouId;
+//     document.getElementById('roomidfield').textContent = data.context.roomId;
+//     document.getElementById('groupidfield').textContent = data.context.groupId;
 
-    // closeWindow call
-    document.getElementById('closewindowbutton').addEventListener('click', function () {
-        liff.closeWindow();
-    });
+//     // openWindow call
+//     document.getElementById('openwindowbutton').addEventListener('click', function () {
+//         liff.openWindow({
+//             url: 'https://line.me'
+//         });
+//     });
 
-    // sendMessages call
-    document.getElementById('sendmessagebutton').addEventListener('click', function () {
-        liff.sendMessages([{
-            type: 'text',
-            text: "You've successfully sent a message! Hooray!"
-        }, {
-            type: 'sticker',
-            packageId: '2',
-            stickerId: '144'
-        }]).then(function () {
-            window.alert("Message sent");
-        }).catch(function (error) {
-            window.alert("Error sending message: " + error);
-            window.alert('a');
-        });
-    });
+//     // closeWindow call
+//     document.getElementById('closewindowbutton').addEventListener('click', function () {
+//         liff.closeWindow();
+//     });
 
-    //get profile call
-    document.getElementById('getprofilebutton').addEventListener('click', function () {
-        liff.getProfile().then(function (profile) {
-            document.getElementById('useridprofilefield').textContent = profile.userId;
-            document.getElementById('displaynamefield').textContent = profile.displayName;
+//     // sendMessages call
+//     document.getElementById('sendmessagebutton').addEventListener('click', function () {
+//         liff.sendMessages([{
+//             type: 'text',
+//             text: "You've successfully sent a message! Hooray!"
+//         }, {
+//             type: 'sticker',
+//             packageId: '2',
+//             stickerId: '144'
+//         }]).then(function () {
+//             window.alert("Message sent");
+//         }).catch(function (error) {
+//             window.alert("Error sending message: " + error);
+//             window.alert('a');
+//         });
+//     });
 
-            var profilePictureDiv = document.getElementById('profilepicturediv');
-            if (profilePictureDiv.firstElementChild) {
-                profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-            }
-            var img = document.createElement('img');
-            img.src = profile.pictureUrl;
-            img.alt = "Profile Picture";
-            profilePictureDiv.appendChild(img);
+//     //get profile call
+//     document.getElementById('getprofilebutton').addEventListener('click', function () {
+//         liff.getProfile().then(function (profile) {
+//             document.getElementById('useridprofilefield').textContent = profile.userId;
+//             document.getElementById('displaynamefield').textContent = profile.displayName;
 
-            document.getElementById('statusmessagefield').textContent = profile.statusMessage;
-            toggleProfileData();
-        }).catch(function (error) {
-            window.alert("Error getting profile: " + error);
-        });
-    });
-}
+//             var profilePictureDiv = document.getElementById('profilepicturediv');
+//             if (profilePictureDiv.firstElementChild) {
+//                 profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
+//             }
+//             var img = document.createElement('img');
+//             img.src = profile.pictureUrl;
+//             img.alt = "Profile Picture";
+//             profilePictureDiv.appendChild(img);
 
-function toggleProfileData() {
-    var elem = document.getElementById('profileinfo');
-    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
-        elem.style.display = "none";
-    } else {
-        elem.style.display = "block";
-    }
-}
+//             document.getElementById('statusmessagefield').textContent = profile.statusMessage;
+//             toggleProfileData();
+//         }).catch(function (error) {
+//             window.alert("Error getting profile: " + error);
+//         });
+//     });
+// }
+
+// function toggleProfileData() {
+//     var elem = document.getElementById('profileinfo');
+//     if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
+//         elem.style.display = "none";
+//     } else {
+//         elem.style.display = "block";
+//     }
+// }
