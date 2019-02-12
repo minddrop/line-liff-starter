82f91e4fadb2b95e69d670a09fd3a202b74f42f6
ryu <ryooooooo1@gmail.com>
2019-02-12 17:36:35 +0900

    test

--- src/liff-starter.js
+++ src/liff-starter.js
@@ -1,59 +1,67 @@
+console.log(
+  firebase
+    .storage()
+    .ref()
+    .child('a.txt').fullPath
+)
+document.addEventListener('DOMContentLoaded', function() {
+  const chatTestCol = firebase.firestore().collection('chat-test')
+  const chatBoard = document.querySelector('#board')
 
-document.addEventListener("DOMContentLoaded", function () {
-    const chatTestCol = firebase.firestore().collection("chat-test");
-    const chatBoard = document.querySelector("#board");
+  // const songs = ["a", "b"];
+  // const fragment = document.createDocumentFragment();
+  // for (let i = 0, len = songs.length; i < len; i++) {
+  //     const li = document.createElement("li");
+  //     const nyou = document.createElement("li");
+  //     const song = document.createTextNode(songs[i]);
+  //     li.appendChild(song);
+  //     nyou.appendChild(document.createTextNode("name:minddrop"));
+  //     fragment.appendChild(li); // fragmentの追加する
+  //     fragment.appendChild(nyou);
+  // }
+  // chatBoard.appendChild(fragment);
 
-    // const songs = ["a", "b"];
-    // const fragment = document.createDocumentFragment();
-    // for (let i = 0, len = songs.length; i < len; i++) {
-    //     const li = document.createElement("li");
-    //     const nyou = document.createElement("li");
-    //     const song = document.createTextNode(songs[i]);
-    //     li.appendChild(song);
-    //     nyou.appendChild(document.createTextNode("name:minddrop"));
-    //     fragment.appendChild(li); // fragmentの追加する
-    //     fragment.appendChild(nyou);
-    // }
-    // chatBoard.appendChild(fragment);
-
-    chatTestCol.onSnapshot(querySnapshot => {
-        const fragment = document.createDocumentFragment();
-        chatTestCol.get().then(querySnapshot => {
-            querySnapshot.forEach(doc => {
-                data = doc.data();
-                const name = document.createElement("li");
-                const message = document.createElement("li");
-                const date = document.createElement("li");
-                name.appendChild(document.createTextNode("name: " + data.name));
-                date.appendChild(document.createTextNode("date: " + new Date(data.date)));
-                message.appendChild(document.createTextNode(data.body));
-                fragment.appendChild(date);
-                fragment.appendChild(name);
-                fragment.appendChild(message);
-            });
-            chatBoard.appendChild(fragment);
-        });
+  chatTestCol.onSnapshot(querySnapshot => {
+    const fragment = document.createDocumentFragment()
+    chatTestCol.get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        data = doc.data()
+        const name = document.createElement('li')
+        const message = document.createElement('li')
+        const date = document.createElement('li')
+        name.appendChild(document.createTextNode('name: ' + data.name))
+        date.appendChild(
+          document.createTextNode('date: ' + new Date(data.date))
+        )
+        message.appendChild(document.createTextNode(data.body))
+        fragment.appendChild(date)
+        fragment.appendChild(name)
+        fragment.appendChild(message)
+      })
+      chatBoard.appendChild(fragment)
     })
-    document.querySelector("#submit").addEventListener("click", (event) => {
-        const chatBody = document.querySelector("#body");
-        const chatName = document.querySelector("#name");
-        if (!chatName.value) alert("Enter your name")
-        else if (!chatBody.value) alert("Enter your message");
-        else {
-            chatTestCol.add(
-                {
-                    body: chatBody.value,
-                    date: Date.now(),
-                    name: chatName.value,
-                }
-            ).then(
-                () => {
-                    chatBody.value = "";
-                    chatName.value = "";
-                }
-            ).catch(e => { alert(e); });
-        }
-    })
+  })
+  document.querySelector('#submit').addEventListener('click', event => {
+    const chatBody = document.querySelector('#body')
+    const chatName = document.querySelector('#name')
+    if (!chatName.value) alert('Enter your name')
+    else if (!chatBody.value) alert('Enter your message')
+    else {
+      chatTestCol
+        .add({
+          body: chatBody.value,
+          date: Date.now(),
+          name: chatName.value
+        })
+        .then(() => {
+          chatBody.value = ''
+          chatName.value = ''
+        })
+        .catch(e => {
+          alert(e)
+        })
+    }
+  })
 })
 
 // window.onload = function (e) {
